f86b2e313b71282d0dbac97eb4e9ac9ced0a10fd
PMI EDUVERSE <pmieduverse@gmail.com>
2025-04-04 11:56:05 +0530

    Update pmiedupage.js

--- pmiedupage.js
+++ pmiedupage.js
@@ -1,6 +1,6 @@
 const studentData = {
     students: [
-        { name: "Pankaj Meena", rollNo: "7357" },
+        { name: "Pankaj Meena", rollNo: "PMIE001" },
         { name: "Sneha Patel", rollNo: "PMIE004" },
         { name: "Amit Kumar", rollNo: "PMIE005" },
         { name: "Priya Sharma", rollNo: "PMIE006" },
